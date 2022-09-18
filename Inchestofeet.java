1cdad9b6573d1545dcb2b50ab638fb023a64bd11
Benny Van Siclen <105946108+VanThaMan180@users.noreply.github.com>
2022-09-17 20:43:20 -0400

    Update Inchestofeet.java

--- Inchestofeet.java
+++ Inchestofeet.java
@@ -1,16 +1,21 @@
 package Week2;
 
+import java.util.Scanner;
+
 public class Inchestofeet {
 
 	public static void main(String[] args) {
-		int inches = 12
-			
-		Scanner = userinput new Scanner(System.in)
-		System.out.println(" Please enter the amount of inches you require ")
-		int userAcknowledgment = userInt.nextline();
-		System.out.println( " The number of feet for the requested inches is " + (userAcknowledgement/inches));
-		
+		inchestoFeet();
+	}
 
+	private static void inchestoFeet() {
+		int inches = 12;
+				
+		try (Scanner userinput = new Scanner(System.in)) {
+			System.out.println(" Please enter the amount of inches you require ");
+			int userAcknowledgment = userinput.nextInt();
+			System.out.println( " The number of feet for the requested inches is " + (userAcknowledgment/inches));
+		}
 	}
 
 }
